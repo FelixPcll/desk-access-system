26ad94071c4c5a4d9bc7cbe89f206ded1ccdd1f4
FelixPcll <felix@df.ufscar.br>
2018-10-17 08:30:51 -0300

    Added Chart JS Graph in Statistics tab

--- menu/stats.js
+++ menu/stats.js
@@ -2,12 +2,66 @@ $(document).ready(function () {
 
     const app = require('electron').remote.app;
     const { remote } = require('electron');
-
+    const Chart = require('chart.js');
 
 
     $.get("./menu/catraca.html", function (catraca) {
         $("#catraca").replaceWith(catraca);
     });
+
+
+
+
+
+
+
+
+    var ctx = $("#myChart");
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun"],
+            datasets: [{
+                label: 'Homens',
+                data: [
+                    81,
+                    37,
+                    72,
+                    98,
+                    64,
+                    22
+                ],
+                backgroundColor: 'rgb(75, 77, 180)',
+                borderColor: 'rgb(125, 127, 255)',
+                fill: false
+            },
+            {
+                label: 'Mulheres',
+                data: [
+                    89,
+                    30,
+                    64,
+                    103,
+                    69,
+                    17
+                ],
+                backgroundColor: 'rgb(247, 99, 247)',
+                borderColor: 'rgb(255, 155, 255)',
+                fill: false,
+                pointRadius: 5,
+                showLine: false
+            }]
+        },
+        options: {
+            elements: {
+                point: {
+                    pointStyle: "rectRot"
+                }
+            }
+        }
+    });
+
 
 
 
